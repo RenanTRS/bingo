d8ab2e390be76c03209b6bb9a9a8c25c97dd3460
RenanTRS <renantrsofficial@gmail.com>
2019-07-18 22:35:33 -0400

    Primeiro commit

---
+++ bingo.py
@@ -0,0 +1,62 @@
+from random import randint
+from time import sleep
+
+opc = '' #Opção
+most = '' #Mostrar
+qtd = cont = contTemp = 0 #Quantidade, Contador, Contador temporário.
+cartela = [] #Cartela
+tabTemp = [] #Tabela temporária.
+
+while True:
+
+    pedra = randint(1, 99) # Gerar pedra aleatória.
+    if pedra not in cartela: # Se a pedra não estiver faça...
+        print('Pedra...   ', end=' ')
+
+        sleep(1.5)
+        print(pedra)
+        cartela.append(pedra) # Inclua pedra na list da cartela
+        qtd = len(cartela) # Tamanho da cartela
+        
+        #Condição de Saída-----------------------------------------------------
+        opc = str(input('Deseja Continuar? [S/N] ')).strip().upper()[0]
+        if opc == 'N':
+            break
+        
+        #Condição para ver o progresso ---------------------------------------------
+        most = str(input('Deseja ver as pedras cantadas? [S/N] ')).strip().upper()[0]
+        if most == 'S':
+            tabTemp.clear() #Limpar a lista anterior para não gerar valor duplicado.
+            tabTemp = cartela[:]
+            tabTemp.sort()
+            
+            #Laço do progresso---------------------------------------------------------------
+            for p in tabTemp:
+                contTemp += 1
+                if contTemp % 10 == 0: # Se o número for dezena, imprima e depois pule uma linha.
+                    print(f'{p:3} \n')
+                else:
+                    print(f'{p:3}', end=' ') # Se não, apenas imprima do lado
+            
+            print('\n')
+            
+            #Condição de Saída--------------------------------------------------------------------
+            opc = str(input('Deseja continuar? [S/N] ')).strip().upper()[0]
+            if opc == 'N':
+                break
+
+    #Condição do limite de saída----------------------------------------------------------
+    if qtd > 98:
+        break
+
+#Saída-------------------------------------------------------------------------------------------
+tabela = cartela[:]
+tabela.sort()
+for c in tabela:
+    cont += 1
+    if cont % 10 == 0:# Se for dezena, imprima e pule uma linha.
+        print(f'{c:3} \n')
+    else:
+        print(f'{c:3}', end=' ') # Se não, imprima e continue na mesma linha.
+
+print('\n')
